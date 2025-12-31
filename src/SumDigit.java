153f4bdde7a5209142fecaa1c01c6b2cedb454b8
pratiksha-dighe123 <dpratiksha292@gmail.com>
2025-12-31 22:40:03 +0530

    Added Sum of Digit program

---
+++ src/SumDigit.java
@@ -0,0 +1,21 @@
+public class SumDigit {
+
+    int digSum(int num){
+        int digitSum=0;
+
+        while( num >0){
+            int  lastDigit= num % 10;
+            num= num /10;
+
+            digitSum += lastDigit;
+        }
+        return digitSum;
+    }
+
+    public static void main(String[] args){
+        SumDigit sm= new SumDigit();
+        System.out.println(sm.digSum(2345));
+    }
+
+
+}
