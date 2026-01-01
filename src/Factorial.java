b189a6644e54ac1642cc765a554d2dd57cd71134
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-01-01 13:51:50 +0530

    Added nCr binomial coefficient program

--- src/Factorial.java
+++ src/Factorial.java
@@ -6,10 +6,17 @@ public class Factorial {
             }
             return fact;
         }
+
+        int nCr(int n, int r){
+            int fact_n = countFact(n);
+            int fact_r = countFact(r);
+            int fact_nmr = countFact(n-r);
+
+            return fact_n / (fact_r * fact_nmr);
+        }
+
     public static void main(String[] args){
             Factorial f= new Factorial();
-            System.out.println(f.countFact(6));
+            System.out.println(f.nCr(8,2));
     }
-
-
 }
